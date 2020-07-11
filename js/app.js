275e751f4b7400fc453bd6d9b1b99a8e51101c76
Randy Gohmann <rgohma2@gmail.com>
2020-07-11 09:16:10 -0500

    made token and row classes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 console.log('Hello World');
+
+class Token {
+
+	constructor(player, zone, played) {
+		this.player = player // which player the token belongs to
+		this.zone = zone // which zone the token is in 
+		this.played = false // has the token been played yet
+	}
+
+} 
+
+class Row {
+
+	constructor(number, full) {
+		this.number = number // where the row is in relation to the others
+		this.full = false // is the row full with tokens or not
+	}
+
+}
+
+
+
+const game = {
+
+}
